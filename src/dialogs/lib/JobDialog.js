9b337792a4311db1090526cf5357db41610d868f
Igor Pavlenko <ipavlenko@inbox.ru>
2017-11-15 15:46:31 +0500

    Apply wired

--- src/dialogs/lib/JobDialog.js
+++ src/dialogs/lib/JobDialog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 
+import { BACKEND } from 'src/endpoints'
 import { ModalDialog } from 'src/components'
 import { modalsClose } from 'src/store'
 
@@ -21,18 +22,34 @@ export class JobDialog extends React.Component {
         <style jsx>{styles}</style>
         <div className='root job-dialog'>
           <div className='text' dangerouslySetInnerHTML={{ __html: job.details}}></div>
-          <form>
+          <form ref={el => this.formElement = el} onSubmit={e => this.handleSubmit(e)}>
             <div className='field'>
+              <input type='text' id='apply-name' required
+                ref={el => this.nameElement = el}
+                onChange={e => this.handleInput(e.currentTarget)}
+              />
               <label htmlFor='apply-name'>Your name*</label>
-              <input type='text' id='apply-name' required />
             </div>
             <div className='field'>
+              <input type='email' id='apply-email' required
+                ref={el => this.emailElement = el}
+                onChange={e => this.handleInput(e.currentTarget)}
+              />
               <label htmlFor='apply-email'>Email*</label>
-              <input type='email' id='apply-email' required />
             </div>
             <div className='field'>
+              <input type='text' id='apply-phone' required
+                ref={el => this.phoneElement = el}
+                onChange={e => this.handleInput(e.currentTarget)}
+              />
+              <label htmlFor='apply-phone'>Phone*</label>
+            </div>
+            <div className='field'>
+              <textarea id='apply-message' required
+                ref={el => this.messageElement = el}
+                onChange={e => this.handleInput(e.currentTarget)}
+              ></textarea>
               <label htmlFor='apply-message'>Additional information, links: portfolio url, linkedin, github, telegram, skype etc.</label>
-              <textarea id='apply-message' required></textarea>
             </div>
             <div className='buttons'>
               <input className='button' type='submit' value='Send' />
@@ -41,6 +58,28 @@ export class JobDialog extends React.Component {
         </div>
       </ModalDialog>
     )
+  }
+
+  handleInput (el) {
+    el.classList.toggle('not-empty', el.value !== '')
+  }
+
+  async handleSubmit (e) {
+    e.preventDefault()
+
+    // TODO: Move to redux
+    await BACKEND.post('applications', {
+      job: this.props.job._id,
+      name: this.nameElement.value,
+      email: this.emailElement.value,
+      phone: this.phoneElement.value,
+      message: this.messageElement.value
+    })
+    for (const el of [this.nameElement, this.emailElement, this.messageElement]) {
+      el.value = ''
+      this.handleInput(el)
+    }
+    this.props.onClose()
   }
 }
 
