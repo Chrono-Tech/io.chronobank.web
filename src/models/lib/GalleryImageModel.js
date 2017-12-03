547d7679d3c483c68b204d19880533e9f9f8a972
Igor Pavlenko <ipavlenko@inbox.ru>
2017-12-03 19:31:47 +0500

    CMS-28 Replace CloudinaryImages type with a set of nested CloudinaryImage instances

---
+++ src/models/lib/GalleryImageModel.js
@@ -0,0 +1,29 @@
+import assert from 'assert'
+import ImageModel from './ImageModel'
+
+export default class GalleryImageModel {
+  constructor ({ id, title, image }) {
+    this.id = id
+    this.title = title
+    assert(image == null || image instanceof ImageModel)
+    this.image = image
+    Object.freeze(this)
+  }
+
+  static fromJS (data) {
+    return data == null ? data : new GalleryImageModel({
+      ...data,
+      image: ImageModel.fromJS(data.image)
+    })
+  }
+
+  static fromServerModel (data) {
+    return data == null ? data : new GalleryImageModel({
+      id: data._id,
+      title: data.title,
+      image: data.image
+        ? ImageModel.fromServerModel(data.image)
+        : null
+    })
+  }
+}
--- src/partials/lib/GallerySection.js
+++ src/partials/lib/GallerySection.js
@@ -72,7 +72,7 @@ export default class GallerySection extends React.Component {
           <div className='swiper-wrapper'>
             {galleries.map(g => (
               (g.images || []).map(i => (
-                <img key={`${g.id}/${i.id}`} className='swiper-slide' src={i.url} />
+                <img key={`${g.id}/${i.id}`} className='swiper-slide' src={i.image.url} />
               ))
             ))}
           </div>
