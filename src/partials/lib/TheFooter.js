31462554937fe2e8388fb748da3821b7a0e552e3
Igor Pavlenko <ipavlenko@inbox.ru>
2018-01-19 01:23:51 +0500

    Copyright updated

--- src/partials/lib/TheFooter.js
+++ src/partials/lib/TheFooter.js
@@ -154,7 +154,7 @@ export default class TheFooter extends React.Component {
               </form>
             </div>
           </div>
-          <address>Copyright ©2017 Edway Group Pty. Ltd. All Rights Reserved.</address>
+          <address>Copyright ©2017 Labor X Australia Pty Ltd. All Rights Reserved.</address>
         </div>
       </footer>
     )
