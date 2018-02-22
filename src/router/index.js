ba4c8b7469d1aaedfa8da85109fe6c8c67916062
Igor Pavlenko <ipavlenko@inbox.ru>
2018-02-23 03:20:02 +0500

    eslint updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
 const routes = module.exports = require('next-routes')()
 
-try {
-  routes
-    .add('index', '/')
-    .add('team', '/team')
-    .add('faq', '/faq')
-    .add('products-details', '/products/:slug')
-} catch(e) {
-  console.log('router error', e)
-}
+routes
+  .add('index', '/')
+  .add('team', '/team')
+  .add('faq', '/faq')
+  .add('products-details', '/products/:slug')
